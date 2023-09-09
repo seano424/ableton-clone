e2e90fe8f6a7544c7796cfa4062d1a28418ea372
seano424 <soreilly424@gmail.com>
2023-09-09 17:22:39 -0500

    updating prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>hello</p>
+    <main className="flex flex-col items-center justify-between p-24 min-h-screen">
+
     </main>
   )
 }
